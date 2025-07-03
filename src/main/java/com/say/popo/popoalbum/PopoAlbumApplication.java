a865e96ce1cde2bf1db052a08374276db6b25031
poposay <saypom1659@gmail.com>
2025-07-03 22:10:12 +0900

    git add . git commit -m "Init project: Set up Spring Boot with Maven, dependencies, and gitignore"

---
+++ src/main/java/com/say/popo/popoalbum/PopoAlbumApplication.java
@@ -0,0 +1,13 @@
+package com.say.popo.popoalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopoAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopoAlbumApplication.class, args);
+	}
+
+}
