45baea83d9339f513c7f76f0576ef9fa48e0e035
poposay <saypom1659@gmail.com>
2025-07-03 22:10:12 +0900

    first commit: Spring Boot プロジェクトの基本構成を作成

---
+++ src/test/java/com/say/popo/popoalbum/PopoAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.say.popo.popoalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopoAlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
