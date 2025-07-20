7aacc8903789553f9bf3f1121d9b6f0c580fd987
poposay <saypom1659@gmail.com>
2025-07-20 16:10:39 +0900

    Implement signup validation and database persistence

---
+++ src/main/java/com/say/popo/popoalbum/service/UserService.java
@@ -0,0 +1,35 @@
+package com.say.popo.popoalbum.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.say.popo.popoalbum.dto.RegisterRequest;
+import com.say.popo.popoalbum.entity.Users;
+import com.say.popo.popoalbum.repository.UserRepository;
+
+
+@Service
+public class UserService {
+
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
+	
+	public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
+	
+	public void register(RegisterRequest request) {
+		Users user = new Users();
+		user.setUsername(request.getUsername());
+		user.setEmail(request.getEmail());
+		String rawPassword = request.getPassword();
+		String hashedPassword = passwordEncoder.encode(rawPassword);
+		user.setPassword_hash(hashedPassword);
+		user.setLast_login(LocalDateTime.now());
+		
+		userRepository.save(user);
+	}
+}
