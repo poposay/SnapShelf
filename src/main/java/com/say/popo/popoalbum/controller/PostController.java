bcd0abc73b15769d57c00a8eee1e3ae576aeacdd
asa <saypom1659@gmail.com>
2025-09-02 22:49:55 +0900

    add:初投稿後のホーム遷移と2回目以降の投稿画面の表示ロジックを実装

---
+++ src/main/java/com/say/popo/popoalbum/controller/PostController.java
@@ -0,0 +1,21 @@
+package com.say.popo.popoalbum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class PostController {
+
+	@GetMapping("/post")
+	public String showPostPage() {
+		return "/post";
+	}
+	
+	@PostMapping("/post")
+	public void uploadPost(@RequestParam("memory") MultipartFile file ,@RequestParam String userComment) {
+		System.out.println("投稿完了");
+	}
+}
