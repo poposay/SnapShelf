4a2d384705ff447b2dc27446037664f56554e4b7
asa <saypom1659@gmail.com>
2025-09-03 23:26:04 +0900

    feat: ControllerAdviceによる共通セッション取得を実装
    -ControllerAdviceクラスを追加し、セッションからログインユーザー情報を取得する共通ロジックを実装
    -ModelAttributeでユーザー情報を全画面に渡せるように設定
    -ヘッダーにログインユーザー名を表示するUIを追加
    -ホーム画面のUIを更新

---
+++ src/main/java/com/say/popo/popoalbum/advice/GlobalControllerAdvice.java
@@ -0,0 +1,23 @@
+package com.say.popo.popoalbum.advice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.say.popo.popoalbum.entity.Users;
+
+import jakarta.servlet.http.HttpSession;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+	
+	@ModelAttribute("currentUser")
+	public Users getCurrentUser(HttpSession session) {
+		return (Users) session.getAttribute("currentUser");
+	}
+	
+	@ModelAttribute("userId")
+	public Long getUserId(HttpSession session) {
+		return (Long) session.getAttribute("userId");
+	}
+
+}
