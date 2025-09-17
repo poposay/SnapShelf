999f531d3c7548416b37da03f5e7631d8a8701b6
asa <saypom1659@gmail.com>
2025-09-17 23:06:08 +0900

    add: アルバムページでの過去の投稿表示機能 & AIコメントのDB保存機能を実装

--- src/main/java/com/say/popo/popoalbum/repository/AICommentRepository.java
+++ src/main/java/com/say/popo/popoalbum/repository/AICommentRepository.java
@@ -2,7 +2,10 @@ package com.say.popo.popoalbum.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.say.popo.popoalbum.entity.AIComment;
+import com.say.popo.popoalbum.entity.Post;
 
 public interface AICommentRepository extends JpaRepository<AIComment, Long> {
+	
+	AIComment findByPost(Post post);
 
 }
