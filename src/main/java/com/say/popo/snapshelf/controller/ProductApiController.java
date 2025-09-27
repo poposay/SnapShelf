81903ba71564f01ca12445b3cbaa17d832e95ee9
asa <saypom1659@gmail.com>
2025-09-27 22:28:33 +0900

    feat:商品一覧画面にページングと検索機能を追加
    ・SpringBoot側でPageableを使ったページング処理を実装
    ・Vueでaxiosを使ったページ切り替え機能を追加

--- src/main/java/com/say/popo/snapshelf/controller/ProductApiController.java
+++ src/main/java/com/say/popo/snapshelf/controller/ProductApiController.java
@@ -1,16 +1,28 @@
 package com.say.popo.snapshelf.controller;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.say.popo.snapshelf.dto.ProductDto;
+import com.say.popo.snapshelf.entity.Product;
+import com.say.popo.snapshelf.repository.ProductRepository;
 import com.say.popo.snapshelf.service.ProductCreateService;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Page;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
@@ -21,6 +33,12 @@ public class ProductApiController {
 
 	@Autowired
 	private ProductCreateService productCreateService;
+	private final ProductRepository productRepository;
+	
+	public ProductApiController(ProductCreateService productCreateService,ProductRepository productRepository) {
+		this.productCreateService = productCreateService;
+		this.productRepository = productRepository;
+	}
 	
 	@PostMapping("/update-description")
 	public ResponseEntity<?> updateDescription(HttpEntity<String> httpEntity) {
@@ -49,4 +67,13 @@ public class ProductApiController {
 			return ResponseEntity.internalServerError().build();
 		}
 	}
+	
+	@GetMapping("/products")
+	public Page<ProductDto> getAllProducts(@RequestParam(defaultValue = "0") int page,
+											@RequestParam(defaultValue = "10") int size) {
+		Pageable pageable = PageRequest.of(page, size);
+		return productRepository.findAll(pageable)
+				.map(ProductDto::new);
+	}
+	
 }
