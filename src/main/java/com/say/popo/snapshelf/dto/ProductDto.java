7c9c6a9d741500e6f9e3d2c69a4f377d348ca702
asa <saypom1659@gmail.com>
2025-09-27 22:28:33 +0900

    feat:商品一覧画面にページングと検索機能を追加
    ・SpringBoot側でPageableを使ったページング処理を実装
    ・Vueでaxiosを使ったページ切り替え機能を追加

---
+++ src/main/java/com/say/popo/snapshelf/dto/ProductDto.java
@@ -0,0 +1,59 @@
+package com.say.popo.snapshelf.dto;
+
+import com.say.popo.snapshelf.entity.Product;
+
+public class ProductDto {
+	private Long id;
+	private String product_name;
+	private int price;
+	private int stock;
+	private String imageUrl;
+	
+	public ProductDto(Product product) {
+		this.id = product.getId();
+		this.product_name = product.getProduct_name();
+		this.price = product.getPrice();
+		this.stock = product.getStock();
+		this.imageUrl = product.getImage_url();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProduct_name() {
+		return product_name;
+	}
+
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+}
